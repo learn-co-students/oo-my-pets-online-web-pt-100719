b5ff979799b189eaab3084310f64b3778002dc73
Adrian Bruno <adrian.j.bruno@gmail.com>
2019-11-17 23:25:14 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
-  # code goes here
+
+  attr_reader :name # Name does not change
+  attr_accessor :owner, :mood
+  @@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @mood = mood
+    @name = name
+    @owner = owner
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @pets = {cats: [], dogs: []}
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
 end
