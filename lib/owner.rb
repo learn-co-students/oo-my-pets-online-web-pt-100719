2e24727bc74487d11203bdaf459fc2d39e1500c3
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-21 23:24:01 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,11 @@
 class Owner
-  # code goes here
+  attr_accessor :pets 
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    #@species = species
+  end 
 end
